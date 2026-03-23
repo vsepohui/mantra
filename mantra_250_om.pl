8e2c2d11127e70e617bf4229b44eff9d73337a85
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-03-23 09:27:54 +0500

    second mantra

---
+++ mantra_250_om.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use 5.022;
+use warnings;
+
+$| = 1;
+
+my @say = qw/Hare Krishna Rama Om/;
+my @seq = ([qw/0 1 0 1 3/], [qw/0 2 0 2 3/], [qw/3/x5]);
+my @rythm = qw/0 1 0 1 2/;
+
+for (2) {
+	my $c = 1;
+	for my $r (@rythm) {
+		for (1..5) {
+			for (@{$seq[$r]}) {
+				print $say[$_] . ($c ++ % 5 == 0 ? "!\n" : ' ');
+				sleep 1;
+			}
+		}
+		say '';		
+	}
+}
+
+1;
