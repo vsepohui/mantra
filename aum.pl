342976ed990c4911983ca217fcb1ae2300172584
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-03-23 09:44:46 +0500

    Add Aum mantra

---
+++ aum.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use 5.022;
+use warnings;
+
+$| = 1;
+
+my @say = qw/Aum Mani Padme Hum/;
+
+for (1..8) {
+	my $c = 1;
+	for (@say) {
+		print $_ . ($c ++ % scalar @say == 0 ? "!\n" : ' ');
+		sleep 1;		
+	}
+}
+
+1;
