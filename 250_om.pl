d436e4b737d8b453d17323d6736bf74c9228dd92
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-03-23 09:38:58 +0500

    typo fix

--- 250_om.pl
+++ 250_om.pl
@@ -7,11 +7,11 @@ $| = 1;
 
 my @say = qw/Hare Krishna Rama Om/;
 my @seq = ([qw/0 1 0 1 3/], [qw/0 2 0 2 3/], [qw/3/x5]);
-my @rythm = qw/0 1 0 1 2/;
+my @rhythm = qw/0 1 0 1 2/;
 
 for (1..2) {
 	my $c = 1;
-	for my $r (@rythm) {
+	for my $r (@rhythm) {
 		for (1..5) {
 			for (@{$seq[$r]}) {
 				print $say[$_] . ($c ++ % 5 == 0 ? "!\n" : ' ');
