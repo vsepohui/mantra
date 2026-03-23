ea87e2b41e672d37eb617c1fb0f55c99902df20b
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-03-23 08:52:19 +0500

    update

--- mantra.pl
+++ mantra.pl
@@ -11,12 +11,7 @@ my $seq = '0101110002022200';
 for (1..108) {
 	my $c = 0;
 	for (split //, $seq) {
-		print $say[$_];
-		if ($c % 2) {
-			say "!";
-		} else {
-			print ' ';
-		}
+		print $say[$_] . ($c % 2 ? "!\n" : ' ');
 		sleep 1;
 		$c ++;
 	}
